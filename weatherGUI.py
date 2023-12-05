e7d799e900543ca7eef5e39179a059383fd8c35c
ArmaniCodes <armanibep@gmail.com>
2023-12-04 22:04:16 -0500

    Setup design for GUI

--- weatherGUI.py
+++ weatherGUI.py
@@ -6,7 +6,24 @@ from request import getCityWeather
 class WeatherGui:
     def __init__(self):
         self.root = ctk.CTk()
+
+
+
+        #Used to check if the main Frame is visualized
         self.visualized = False
+
+        self.setUpGui()
+
+        #main loop that runs the GUI
         self.root.mainloop()
+
+
+    def setUpGui(self):
+        ctk.set_appearance_mode("dark")
+        ctk.set_default_color_theme("dark-blue")
+        self.root.resizable(False,False)
+        self.root.title("Weather Lookup")
+        self.root.geometry('500x400')
+
 
 
