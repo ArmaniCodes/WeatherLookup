f8211adeb5fe03740bd6c128b7ce2d206c1097ee
ArmaniCodes <armanibep@gmail.com>
2023-12-04 21:39:31 -0500

    Add method to check response

--- request.py
+++ request.py
@@ -8,9 +8,12 @@ params = {  f"access_key" : {apiK} ,
             "query":""}
 
 
+def checkResponse(response):
+    if "success" in response.json() or response.status_code != 200:
+        return None
+
+
 def getCityWeather(City):
     params["query"] = City
     response = requests.get(apiURL,params=params)
-    print(reponse.json())
-
-getCityWeather("New York")
+    return checkResponse(response)
