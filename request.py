db8e5e1e346e9ed0b522fc8fea14c9a1adf70687
ArmaniCodes <armanibep@gmail.com>
2023-12-04 21:36:39 -0500

    Add test request

--- request.py
+++ request.py
@@ -6,3 +6,11 @@ apiK = config.apiKey
 apiURL = "http://api.weatherstack.com/current"
 params = {  f"access_key" : {apiK} ,
             "query":""}
+
+
+def getCityWeather(City):
+    params["query"] = City
+    response = requests.get(apiURL,params=params)
+    print(reponse.json())
+
+getCityWeather("New York")
