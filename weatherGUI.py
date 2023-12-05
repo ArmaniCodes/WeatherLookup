211cd71cf0a266758e62feb1456d2653f2cb7a22
ArmaniCodes <armanibep@gmail.com>
2023-12-04 22:13:22 -0500

    Add textbox, textboxVar to hold textbox text, and add button to search

--- weatherGUI.py
+++ weatherGUI.py
@@ -30,6 +30,14 @@ class WeatherGui:
         guiTitle = ctk.CTkLabel(self.root,text="Weather Lookup", font=("Arial",15))
         guiTitle.pack()
 
+        #Variable to store textBox text
+        self.city_input = tk.StringVar()
+        city_entry = ctk.CTkEntry(self.root,textvariable= self.city_input)
 
+        #Button to search
+        self.city_entryb = ctk.CTkButton(self.root,  text = "Search Weather", fg_color="#85338F", command = self.searchCity)
+
+        city_entry.pack()
+        self.city_entryb.pack(pady=10)
 
 
